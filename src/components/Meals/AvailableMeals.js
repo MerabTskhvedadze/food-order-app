44ab98225a282f51c84a9c951f709c5ca0a23a39
MerabUN <merabi.tskhvedadze@gmail.com>
2022-06-15 21:12:57 +0400

    added form, cart and still adding Modal

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,7 +32,8 @@ const DUMMY_MEALS = [
 function AvailableMeals() {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
-      key={meal.key}
+      id={meal.id}
+      key={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,30 @@
+import ReactDom from "react-dom";
+import classes from "./Modal.module.css";
+
+function BackDrop(props) {
+  return <div className={classes.backDrop} />;
+}
+
+function ModalOerlay(props) {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.cnotent}>{props.children}</div>
+    </div>
+  );
+}
+
+const portalElement = document.getElementById("overlays");
+
+function Modal(props) {
+  return (
+    <>
+      {ReactDom.createPortal(<BackDrop />, portalElement)}
+      {ReactDom.createPortal(
+        <ModalOerlay>{props.children}</ModalOerlay>,
+        portalElement
+      )}
+    </>
+  );
+}
+
+export default Modal;

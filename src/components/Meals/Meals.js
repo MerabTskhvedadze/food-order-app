919544e1081b3772a210c9ef8575f47b264e4cc5
MerabUN <merabi.tskhvedadze@gmail.com>
2022-06-15 17:03:59 +0400

    added meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsSummery from "./MealsSummery";
+import AvailableMeals from "./AvailableMeals";
+
+function Meals() {
+  return (
+    <>
+      <MealsSummery />
+      <AvailableMeals />
+    </>
+  );
+}
+
+export default Meals;

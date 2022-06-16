e4f27ccb27ac596567b576fda4dca0d9c0293e2f
MerabUN <merabi.tskhvedadze@gmail.com>
2022-06-16 16:50:39 +0400

    adding cart items with reducer

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   };
 
   return (
-    <CartProvider >
+    <CartProvider>
       {isVisible && <Cart onClose={cartUnvisibleHandler} />}
       <Header onVisibleCart={cartVisibleHandler} />
       <main>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../Store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
+
+  const numOfCartItems = cartCtx.items.reduce((curNumber, items) => {
+    return curNumber + items.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onActivate}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   );
 }

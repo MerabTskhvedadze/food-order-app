d32ec942348f6fb85cdbc1cb2255f89ae5d7527d
MerabUN <merabi.tskhvedadze@gmail.com>
2022-06-16 18:39:57 +0400

    now we can add items amount in cart

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,13 @@
-// eslint-disable-next-line no-unused-vars
-import classes from './Input.module.css'
+import React from "react";
+import classes from "./Input.module.css";
 
-function Input(props){
-  return(
+const Input = React.forwardRef((props, ref) => {
+  return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input id={props.input.id} {...props.input}/>
+      <input ref={ref} id={props.input.id} {...props.input} />
     </div>
-  )
-}
+  );
+});
 
-export default Input
+export default Input;
